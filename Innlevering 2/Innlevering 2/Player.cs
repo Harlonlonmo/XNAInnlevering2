9728a66f3775c12f578039033ce089b44c8f1673
Harald Lønmo <harlonlonmo@gmail.com>
2013-03-09 22:32:43 +0100

    addapted to GameObject

--- Innlevering 2/Innlevering 2/Player.cs
+++ Innlevering 2/Innlevering 2/Player.cs
@@ -12,47 +12,45 @@ using C3.XNA;
 
 namespace Innlevering_2
 {
-    class Player
+    class Player : GameObject
     {
-        private Game game;
-        public Vector2 PlayerPosition { get; set; }
         public Point PlayerSize { get; private set; }
         //public Physics physics;
         public float speed = 2f;
         private Room room;
 
 
-        public Player(Game game, Vector2 PlayerPosition, Point PlayerSize, float speed)
+        public Player(Game game, Vector2 PlayerPosition, Point PlayerSize, float speed):base(game)
         {
-            this.PlayerPosition = PlayerPosition;
+            Position = PlayerPosition;
             this.PlayerSize = PlayerSize;
-            this.game = game;
+
 
         }
 
         public void Update(GameTime gameTime, Room room)
         {
-            InputController controller = (InputController)game.Services.GetService(typeof(InputController));
+            InputController controller = (InputController)Game.Services.GetService(typeof(InputController));
             this.room = room;
 
             //physics.Update(gameTime);
 
             //Movement
             if (controller.gamePadState.ThumbSticks.Left.X != 0f)
-                PlayerPosition += new Vector2(controller.gamePadState.ThumbSticks.Left.X * speed, 0);
+                Position += new Vector2(controller.gamePadState.ThumbSticks.Left.X * speed, 0);
         }
 
         public void Draw(SpriteBatch spriteBatch/*, GameTime gameTime*/)
         {
             Primitives2D.FillRectangle(spriteBatch, new Rectangle(
-                (int) PlayerPosition.X, (int) PlayerPosition.Y, 
+                (int) Position.X, (int) Position.Y, 
                 PlayerSize.X, PlayerSize.Y), 
                 Color.Brown);
         }
 
         protected bool Collide()
         {
-            Rectangle playerRect = new Rectangle((int)PlayerPosition.X, (int)PlayerPosition.Y, PlayerSize.X, PlayerSize.Y);
+            Rectangle playerRect = new Rectangle((int)Position.X, (int)Position.Y, PlayerSize.X, PlayerSize.Y);
             Rectangle mapFrameRect = new Rectangle(room.PosX, room.PosY, room.Width, room.Height);
 
             return (playerRect.Intersects(mapFrameRect));
