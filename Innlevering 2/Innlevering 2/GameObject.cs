cf2fcdbf093c3b76cb0b3ef872697504b92b0580
Harald Lønmo <harlonlonmo@gmail.com>
2013-03-09 02:06:18 +0100

    gameobject

---
+++ Innlevering 2/Innlevering 2/GameObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Innlevering_2
+{
+    public abstract class GameObject
+    {
+        public Vector2 Position { get; protected set; }
+
+        public Game Game { get; protected set; }
+
+        private Vector2 position;
+
+
+        public GameObject(Game game)
+        {
+            Game = game;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+
+        public void move(Vector2 delta)
+        {
+            Position += delta;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+        }
+
+    }
+}
