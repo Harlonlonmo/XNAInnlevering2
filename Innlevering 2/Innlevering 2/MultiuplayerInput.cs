6729c5206437561f4f9fceec9fbba5cba6766dda
Harald Lønmo <harlonlonmo@gmail.com>
2013-03-12 01:10:00 +0100

    MP input

---
+++ Innlevering 2/Innlevering 2/MultiuplayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Innlevering_2
+{
+    public class MultiuplayerInput : GameComponent
+    {
+
+        public Dictionary<PlayerIndex, GamePadState> GamePadStates { get; protected set; }
+
+
+        public MultiuplayerInput(Game game)
+            : base(game)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+
+    
+}
